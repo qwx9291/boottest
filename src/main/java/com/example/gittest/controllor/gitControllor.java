9deb8a3e2b5d0d79858e85c612576ba162961a68
qwx9291 <929133580@qq.com>
2020-03-10 16:38:40 +0800

    测试创建本地暂存

--- src/main/java/com/example/gittest/controllor/gitControllor.java
+++ src/main/java/com/example/gittest/controllor/gitControllor.java
@@ -1,12 +1,14 @@
 package com.example.gittest.controllor;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class gitControllor {
 
     @RequestMapping("/say")
+    @ResponseBody
     public String say(){
         return "new git project";
     }
